e9a06acbcfa1ad40128a3716054dd6c2e53848b7
brunolcarli <brunolcarli@gmail.com>
2023-08-06 14:25:50 -0300

    CHANGE version

--- anton/settings/common.py
+++ anton/settings/common.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 
 
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
